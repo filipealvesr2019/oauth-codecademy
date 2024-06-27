ef35aad57a7840511bb5dc43e3f616070d57c2fd
Felipe Alves Romualdo <52980575+filipealvesr2019@users.noreply.github.com>
2024-06-27 16:47:58 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,83 @@
+/* 
+ * Package Imports
+*/
+
+const app = require("express");
+require("dotenv").config();
+const express = require('express');
+const partials = require('express-partials');
+const passport = require("passport");
+
+
+const app = express();
+
+
+/*
+ * Variable Declarations
+*/
+
+const PORT = 3005;
+const GITHUB_CLIENT_ID = process.env.GITHUB_CLIENT_ID;
+const GITHUB_CLIENT_SECRET = process.env.GITHUB_CLIENT_SECRET;
+
+/*
+ * Passport Configurations
+*/
+passport.use(new GitHubStrategy({
+  clientID: GITHUB_CLIENT_ID,
+  clientSecret: GITHUB_CLIENT_SECRET,
+  callbackURL: "http://localhost:3001/auth/google/callback"
+}));
+
+
+
+
+
+
+/*
+ *  Express Project Setup
+*/
+
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+app.use(partials());
+app.use(express.json());
+app.use(express.static(__dirname + '/public'));
+
+
+
+
+/*
+ * Routes
+*/
+
+app.get('/', (req, res) => {
+  res.render('index', { user: req.user });
+})
+
+app.get('/account', (req, res) => {
+  res.render('account', { user: req.user });
+});
+
+app.get('/login', (req, res) => {
+  res.render('login', { user: req.user });
+})
+
+app.get('/logout', (req, res) => {
+  req.logout();
+  res.redirect('/');
+});
+
+
+
+
+/*
+ * Listener
+*/
+
+app.listen(PORT, () => console.log(`Listening on ${PORT}`));
+
+/*
+ * ensureAuthenticated Callback Function
+*/
+
