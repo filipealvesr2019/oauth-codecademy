8074c1d97c9799503f9b1aa4946d1ec25429534a
Felipe Alves Romualdo <52980575+filipealvesr2019@users.noreply.github.com>
2024-06-27 16:51:24 -0300

    feat: add Passport Configurations

--- app.js
+++ app.js
@@ -27,7 +27,10 @@ passport.use(new GitHubStrategy({
   clientID: GITHUB_CLIENT_ID,
   clientSecret: GITHUB_CLIENT_SECRET,
   callbackURL: "http://localhost:3001/auth/google/callback"
-}));
+}), (accessToken, refreshToken, profile, done) => {
+  return done(null, profile);
+
+});
 
 
 
